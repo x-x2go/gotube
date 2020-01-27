0aec2ffe6816c9a466ac32091d65ddfe59bd5c77
2go <xxxvi_@naver.com>
2020-01-27 20:46:09 +0900

    Fake db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 export const join = (req,res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+export const login = (req,res) => res.render("login");
+export const logout = (req,res) => res.render("logout");
 
 export const users = (req,res) => res.render("user");
 export const userDetail = (req,res) => res.render("userDetail");
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:338452,
+        title: 'Big Bunny!',
+        description: 'This is Big Buck Bunny',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"2Go",
+            email:"xxxvi_@go.com"
+        }
+    },
+    {
+        id:666432,
+        title: 'Hello Bunny!',
+        description: 'This is Big Buck Bunny',
+        views: 84,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"2Go",
+            email:"xxxvi_@go.com"
+        }
+    },
+    {
+        id:288753,
+        title: 'funny Bunny!',
+        description: 'This is Big Buck Bunny',
+        views: 114,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"2Go",
+            email:"xxxvi_@go.com"
+        }
+    },
+    {
+        id:194204,
+        title: 'Who is Bunny!',
+        description: 'This is Big Buck Bunny',
+        views: 70,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name:"2Go",
+            email:"xxxvi_@go.com"
+        }
+    }
+];
