eecca22f0d1c454f70e9033a75110e9b7b312ac0
2go <xxxvi_@naver.com>
2020-01-28 15:54:25 +0900

    More Controlers

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,17 @@
 import express from "express";
 import routes from "../routes";
 import { search, home } from "../controllers/videoControllers";
-import { join, login, logout } from "../controllers/userController";
+import { getJoin, postJoin, getLogin, postLogin, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
