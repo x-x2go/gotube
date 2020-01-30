632ed27c68443d99dfbbaec802423b602ea7027f
2go <xxxvi_@naver.com>
2020-01-31 01:24:09 +0900

    +mongoDB

--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
-    {
-        id:338452,
-        title: 'Big Bunny!',
-        description: 'This is Big Buck Bunny',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name:"2Go",
-            email:"xxxvi_@go.com"
-        }
-    },
-    {
-        id:666432,
-        title: 'Hello Bunny!',
-        description: 'This is Big Buck Bunny',
-        views: 84,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name:"2Go",
-            email:"xxxvi_@go.com"
-        }
-    },
-    {
-        id:288753,
-        title: 'funny Bunny!',
-        description: 'This is Big Buck Bunny',
-        views: 114,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name:"2Go",
-            email:"xxxvi_@go.com"
-        }
-    },
-    {
-        id:194204,
-        title: 'Who is Bunny!',
-        description: 'This is Big Buck Bunny',
-        views: 70,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name:"2Go",
-            email:"xxxvi_@go.com"
-        }
-    }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect( process.env.MONGO_URL,
+{
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB (●'◡'●)");
+const handleError = (error) => console.log(`Errir ib DB connection:${error}`)
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,7 +1,12 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`👀Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`👀  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
