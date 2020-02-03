22e85d84eaf5dfdce03adb8cd5c88b4324580607
2go <xxxvi_@naver.com>
2020-02-03 17:05:29 +0900

    +edit video, +delete

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoControllers";
+import { getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo } from "../controllers/videoControllers";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -8,8 +8,12 @@ const videoRouter = express.Router();
 //videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
