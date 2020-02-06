88bf55d5726f9e0812606588cf1c2cfdbc3a9cc6
2go <xxxvi_@naver.com>
2020-02-07 02:09:36 +0900

    +webpack(finished)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,14 +12,14 @@ import routes from "./routes";
 
 const app = express();
 
-app.set('view engine', "pug");
-app.use("/uploads", express.static("uploads"));
 app.use(helmet());
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
 
 app.use(localsMiddleware);
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
 import "../scss/styles.scss";
+
+const somthing = async () => {
+  console.log("something");
+};
