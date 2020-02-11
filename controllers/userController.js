9b8cd9df0ac1af8e04b6dd272cae946ad31039bc
2go <xxxvi_@naver.com>
2020-02-11 14:50:13 +0900

    +passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,44 @@
 import routes from "../routes";
+import User from "../models/User";
 
-export const getJoin = (req,res) =>{
-    res.render("join", {pageTitle: "Join"});
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req,res) =>{
-    const{
-        body: {name, email, password, password2}
-    } = req;
-    if(password !== password2){
-        res.statues(400);
-        res.render("join", {pageTitle: "Join"});
-    }else{
-        //To Do: Register User
-        //To Do: Log user in
-        res.redirect(routes.home);
+export const postJoin = async (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.statues(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
     }
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
 };
-export const getLogin = (req,res) => res.render("login", {pageTitle: "Log In"});
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-}
-export const logout = (req,res) => {
-    //To Do: Process Log Out
-    res.redirect(routes.home);
-}
+  res.redirect(routes.home);
+};
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
 
-export const users = (req,res) => res.render("user", {pageTitle: "User"});
-export const userDetail = (req,res) => res.render("userDetail" , {pageTitle: "User Detail"});
-export const editProfile = (req,res) => res.render("editProfile", {pageTitle: "Edit Profile"});
-export const changePassword = (req,res) => res.render("changePassword", {pageTitle: "Change Password"});
+export const users = (req, res) => res.render("user", { pageTitle: "User" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
