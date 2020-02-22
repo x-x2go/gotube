4f5141a092f33e6140ab0168a9a22f8060bf4ccd
2go <xxxvi_@naver.com>
2020-02-23 00:19:44 +0900

    +video recorder

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,6 +2,33 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordButton");
 const videoPreview = document.getElementById("jsVideoPreview");
 
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  streamObject.getVideoTracks()[0].stop();
+  recordBtn.innerHTML = "Start recording";
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
 const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -12,6 +39,8 @@ const getVideo = async () => {
     videoPreview.muted = true;
     videoPreview.play();
     recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     console.log(error);
     recordBtn.innerHTML = "😥 Can`t record";
