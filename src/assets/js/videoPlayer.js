1078bf1d4fdd7f087642ae9de77e881a88880844
x-x2go <xxxvi_@naver.com>
2020-03-27 19:27:49 +0900

    button us

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -52,6 +52,7 @@ function showCursor() {
 function exitFullScreen() {
   fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScreenBtn.addEventListener("click", goFullScreen);
+  videoPlayer.removeEventListener("mousemove", showCursor);
   if (document.exitFullscreen) {
     document.exitFullscreen();
   } else if (document.webkitExitFullscreen) {
